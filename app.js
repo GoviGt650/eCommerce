b378767a65848cd32a5168dcfa95a465bb7548d2
Govind Jadapalli <govindjadapalli92@gmail.com>
2026-09-06 14:59:04 +0530

    mine

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
+
 const app = express();
 app.use(express.json());
 app.get('/', (req,res) => {
     res.send("Open the server on port URL http://localhost:3000");
 });
-
+app.use()
 export default app;
