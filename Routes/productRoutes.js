9d257adc0a78cc2855119148ff9a685f26da54e4
Govind Jadapalli <govindjadapalli92@gmail.com>
2026-09-12 12:12:20 +0530

    segregated routes

---
+++ Routes/productRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createProduct, updateProductById, getAllProducts, getProductById, deleteProductById } from "../Controllers/adminController.js";
+import{ viewAllPublishedProducts, viewPublishedProduct} from "../Controllers/userControllers.js";
+import { tokenVerify  } from '../middleware/tokenVerify.js';
+const router= express.Router();
+router.post('/', tokenVerify, createProduct);
+router.get('/', tokenVerify, getAllProducts);
+router.get('/:id', tokenVerify, getProductById);
+router.put('/:id', tokenVerify, updateProductById);
+router.delete('/:id', tokenVerify, deleteProductById);
+router.get('/viewAllProduct', tokenVerify, viewAllPublishedProducts);
+router.get('/viewsingleProduct/:id', tokenVerify,viewPublishedProduct);
+export default router;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,25 +1,11 @@
 import express from 'express';
 import {
-    userRegister,
-    login
-} from '../Controllers/authController.js';
-import{ viewAllPublishedProducts, viewPublishedProduct} from "../Controllers/userControllers.js";
-import { createProduct, updateProductById, getAllProducts, getAllUsers, getProductById, deleteProductById, deleteUserById } from "../Controllers/adminController.js";
+    getAllUsers,
+    deleteUserById
+} from '../Controllers/adminController.js';
+
 import { tokenVerify  } from '../middleware/tokenVerify.js';
-
 const router= express.Router();
-router.post('/register',userRegister);
-router.post('/login', login);
-
-router.post('/createproduct', tokenVerify, createProduct);
-router.get('/getproducts', tokenVerify, getAllProducts);
-router.get('/getproducts/:id', tokenVerify, getProductById);
-router.get('/getusers', tokenVerify, getAllUsers);
-router.put('/updateproduct/:id', tokenVerify, updateProductById);
-router.delete('/deleteproduct/:id', tokenVerify, deleteProductById);
-
-router.get('/viewAllProduct', tokenVerify, viewAllPublishedProducts);
-router.get('/viewsingleProduct/:id', tokenVerify,viewPublishedProduct);
-router.delete('/deleteuser/:id', tokenVerify, deleteUserById);
-
+router.get('/', tokenVerify, getAllUsers);
+router.delete('/:id', tokenVerify, deleteUserById);
 export default router;
